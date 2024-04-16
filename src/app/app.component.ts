bc7c75d52a294647e5e6ef9030d0cc1f2f13e2b4
Joyrudra <joyrudrabws07@gmail.com>
2024-04-16 22:13:05 +0530

    WIP

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from './services/api.service';
+import { UserComponent } from './user/user.component';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  imports: [UserComponent],
 })
 export class AppComponent implements OnInit {
   constructor(private apiService: ApiService) {}
